fba5baa5cc259cb755994675d27d0baff3c38fd1
William Felipe <williamfelipe081@gmail.com>
2024-09-04 20:51:16 -0300

    feat - create question migration

---
+++ database/migrations/2024_09_04_231921_create_activities_questions_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities_questions', function (Blueprint $table) {
+            $table->id();
+            $table->text('questionText');
+            $table->string('questionType');
+            $table->unsignedBigInteger('activityId');
+            $table->foreign('activityId')->references('id')->on('activities');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities_questions');
+    }
+};
